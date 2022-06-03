4fdfb8f2107f4dd2c1e7ef93376bf2fd5a0a1539
Marcin <marcinstarzomski@gmail.com>
2022-06-03 23:04:29 +0200

    Add functional input slider

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 let drawingBoard = document.querySelector("#drawingBoard");
+let sliderAmount = document.querySelector('#sliderAmount');
+let square = '';
+let elements = '';  
 
-// Amount of squares
+// Initial canvas 16x16
 let inputAmount = 16;
+console.log(inputAmount);
+elements = inputAmount*inputAmount;
+console.log(elements);
+// Assign grid style and numbers of columns
 let gridTemplateColumns = "repeat(" + inputAmount.toString() +', 1fr)'
-let elements = inputAmount*inputAmount;
-
-function multipleSquares() {
-    for (let i = 0; i < elements; i++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        drawingBoard.appendChild(square);
-    }
-}
 drawingBoard.style.gridTemplateColumns = gridTemplateColumns;
 
-multipleSquares();
+// Create squares within canvas
+for (let i = 0; i <= elements; i++) {
+    square = document.createElement("div");
+    square.classList.add("square");
+    drawingBoard.appendChild(square);
+};
+
+// Slider function: clear board and change size of the canvas
+sliderAmount.addEventListener('change', function(e) {
+    // Clear the drawing board
+    if (elements > 0) {
+        while (drawingBoard.firstChild) {
+            drawingBoard.removeChild(drawingBoard.firstChild)
+        }
+    }
+    // Take the value
+    inputAmount = Number(e.target.value);
+    console.log(inputAmount);
+    elements = inputAmount*inputAmount;
+    console.log(elements);
+    // Assign grid style and numbers of columns
+    gridTemplateColumns = "repeat(" + inputAmount.toString() +', 1fr)'
+    drawingBoard.style.gridTemplateColumns = gridTemplateColumns;
+    
+    // Create new amount of squares within canvas
+    for (let i = 0; i < elements; i++) {
+        square = document.createElement("div");
+        square.classList.add("square");
+        drawingBoard.appendChild(square);
+    };
+});
